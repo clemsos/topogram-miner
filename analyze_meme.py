e01c92d4ff13174fef24b04b63552d1913ac1e3f
clemsos <clement.renaud@gmail.com>
2014-08-25 17:48:37 +0200

    fix path

--- analyze_meme.py
+++ analyze_meme.py
@@ -21,8 +21,8 @@ locale.setlocale(locale.LC_ALL, "")
 
 nlp=NLPMiner()
 
-stoplist=[i.strip() for i in open("lib/stopwords/zh-stopwords","r")]
-stoplist+=[i.strip() for i in open("lib/stopwords/stopwords.txt","r")]
+stoplist=[i.strip() for i in open(os.path.abspath(os.path.join(os.path.dirname(__file__), "lib/stopwords/zh-stopwords"))),"r")]
+stoplist+=[i.strip() for i in open(os.path.abspath(os.path.join(os.path.dirname(__file__), "lib/stopwords/stopwords.txt"))),"r")]
 stoplist+=["转发","微博","说 ","一个","【 ","年 ","转 ","请","＂ ","问题","知道","中 ","已经","现在","说","【",'＂',"年","中","今天","应该","真的","月","希望","想","日","这是","太","转","支持"]
 # stoplist+=["事儿","中国"]
 
